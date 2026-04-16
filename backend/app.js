1f36d3474c6d7b695a502411551ca0f566196769
RicardoB <ricardoboone021@gmail.com>
2026-04-16 03:52:28 -0600

    Fix CORS

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mysql = require('mysql2');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors'); 
 
 const app = express();
 app.use(express.json());
+app.use(cors()); 
 
 let db;
 
